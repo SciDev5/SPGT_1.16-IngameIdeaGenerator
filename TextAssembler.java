229070861aafd0ea8e5646bc8b75ff3970423ba1
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2020-07-08 19:27:47 -0400

    Removed dependence on TextSnippetType
    Also fixed bug where subsnippets at the end of a string failed to load from the config.

---
+++ TextAssembler.java
@@ -0,0 +1,68 @@
+package me.scidev.spigotIdeaGenerator.ideaText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class TextAssembler {
+	
+	private static Map<String,List<TextSnippet>> allTextSnippets;
+	private static Random random;
+
+	public static void init() {
+		// Initialize variables.
+		allTextSnippets = new HashMap<String, List<TextSnippet>>();
+		random = new Random();
+	}
+	
+	public static void addSnippet(TextSnippet snippet) {
+		// Get the list corresponding with the new snippet's type to insert into.
+		String snippetType = snippet.getType();
+		List<TextSnippet> snippetGroup = allTextSnippets.get(snippetType);
+		
+		// If the snippetGroup is null, create it and insert it into the map.
+		if (snippetGroup == null) { 
+			snippetGroup = new ArrayList<TextSnippet>();
+			allTextSnippets.put(snippetType, snippetGroup);
+		}
+		
+		// Add the new snippet if it is not already in the list.
+		if (!snippetGroup.contains(snippet)) 
+			snippetGroup.add(snippet);
+	}
+	
+	private static TextSnippet randomSnippetByType(String type) {
+		// Get the list corresponding with the new snippet's type to insert into.
+		List<TextSnippet> snippetGroup = allTextSnippets.get(type);
+		
+		// If the snippetGroup is null or empty, return null, there are no snippets of this type.
+		if (snippetGroup == null) return null;
+		if (snippetGroup.isEmpty()) return null;
+		
+		// Select a random snippet from the list.
+		int index = random.nextInt(snippetGroup.size());
+		return snippetGroup.get(index);
+	}
+	
+	public static String assembleSnippet(TextSnippet snippetIn) {
+		// Get the replacerTypes of text to substitute into the current snippet.
+		String[] replacerTypes = snippetIn.getReplacerTypes();
+		String[] subSnippetStrings = new String[replacerTypes.length];
+		
+		// Select and fill in random snippets according to the replacerTypes from the snippet.
+		for (int i = 0; i < replacerTypes.length; i++) {
+			TextSnippet subSnippet = randomSnippetByType(replacerTypes[i]);
+			subSnippetStrings[i] = assembleSnippet(subSnippet);
+		}
+		
+		// Assemble the text into the snippet.
+		return snippetIn.spliceTextIn(subSnippetStrings);
+	}
+	
+	// Get a random base snippet.
+	public static TextSnippet randomBaseSnippet() {
+		return TextAssembler.randomSnippetByType("BASE");
+	}
+}
